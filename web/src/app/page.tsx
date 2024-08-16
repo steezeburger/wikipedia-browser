91453212fcf0458be19cd492f42fd18ed73f11c8
Jesse Snyder <jessetsnyder@gmail.com>
2024-08-15 22:32:32 -0600

    GA

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
-import WikipediaBrowser from '../components/WikipediaBrowser';
+import React from "react";
+import WikipediaBrowser from "../components/WikipediaBrowser";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 
 const Home: React.FC = () => {
   return (
     <div>
       <WikipediaBrowser />
+      <GoogleAnalytics gaId="G-XFQJZD9P2K" />
     </div>
   );
 };

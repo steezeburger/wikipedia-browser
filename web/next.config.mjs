e3380ed4959cd1c8ff6c2dd23f3d7e675768b885
Jesse Snyder <jessetsnyder@gmail.com>
2024-08-15 20:50:12 -0600

    deploy static site via github pages?

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/wikipedia-browser",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
